e4d847d2ccac90d18b75ac2ddf1f027e4355947b
Cameron Simmons <cbsimmons4@gmail.com>
2020-04-26 01:59:57 -0400

    finshed chat room

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var socket = require ('socket.io');
+
+// App set up
+var app = express();
+var server = app.listen (4000, function (){
+    console.log('listening to request on port 4000');
+});
+
+//Static files
+app.use(express.static('public'));
+
+//Socket
+var io = socket(server);
+
+io.on('connection', function(socket) {
+
+    console.log ('connection has been made: ' + socket.id);
+
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', function(data){
+        // console.log(data);
+        socket.broadcast.emit('typing',data);
+    });
+
+});
